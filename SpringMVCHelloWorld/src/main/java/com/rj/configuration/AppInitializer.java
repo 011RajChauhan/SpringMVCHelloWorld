3552a5105e217dc8efbe55065edee273cf815464
rajan.ch <rajan.ch@LP-5CD8114QXP.HCLT.CORP.HCL.IN>
2018-08-06 12:43:50 +0530

    updated code

---
+++ SpringMVCHelloWorld/src/main/java/com/rj/configuration/AppInitializer.java
@@ -0,0 +1,27 @@
+package com.rj.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[]{MvcConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String [] {"/"};
+	}
+
+}
